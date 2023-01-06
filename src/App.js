afa4e5648ef42592650395784250b6cb96b0f1bc
MicFrost <116974794+MicFrost@users.noreply.github.com>
2023-01-06 22:39:01 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+export default function Bio() {
+  return (
+    <>
+      <div className="intro">
+        <h1>Welcome to my website: Cook with Cats!</h1>
+      </div>
+      <p className="summary">
+        You can find my Recipes here.
+        <br />
+        <br />
+        <b>
+          And <i>pictures</i>
+        </b>{" "}
+        of Cats!
+      </p>
+    </>
+  );
+}
